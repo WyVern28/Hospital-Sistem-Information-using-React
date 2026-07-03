fd293c12b717d4186ed6e3fa2e657b177ada296b
William Prasetyo Utomo <williamellio05@gmail.com>
2026-07-03 16:40:14 +0700

    bikin routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,10 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({ message: "Backend nyala"});
-});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server berjalan diport http://localhost:${PORT}`);
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
